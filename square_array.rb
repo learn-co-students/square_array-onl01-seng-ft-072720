146c65e38339130354e64e691565a6ee25f7c4a6
Anthony Lepore <violacellonyc@gmail.com>
2020-07-28 19:08:29 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
+# require 'pry'
+
 def square_array(array)
   # your code here
+ squared = []
+  array.each do |arr|
+   calc = arr ** 2
+   # binding.pry
+   squared.push(calc)
+  
+  end
+  array = squared
+  
 end
