930e3ab370f1df991d02b5b2137422159fbb31d6
Alan Coomes <acoomes1@cord.edu>
2020-07-29 23:46:28 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
+
 def square_array(array)
-  # your code here
+  squared_array = []
+  array.each do |number|
+    new_num = number ** 2
+    squared_array.push(new_num)
+  end
+   squared_array
 end
