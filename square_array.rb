47ee8a8d99db15f6707b115cd74df661157513b8
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-07-31 03:49:26 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,18 @@
+# your code here
+#build a method, square_array
+#squares each element in an array of numbers
+#returns a new array of these squared numbers
+
+
+
+
 def square_array(array)
-  # your code here
+
+ new_array = []
+
+ array.each do |integer|
+  new_array << integer ** 2
+ end
+ return new_array
 end
+
